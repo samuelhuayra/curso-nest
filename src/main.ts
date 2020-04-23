4a75533fbeb308377b3a5d5e13de2d5924d8028f
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-23 18:13:14 -0400

    logging section

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('---Start:Logging---');
+  console.log('---End:Logging---');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
