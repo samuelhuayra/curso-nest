b6ffd4776f2feb48cd06d31855b24199d9187236
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-24 19:38:43 -0400

    cors for prod

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,12 @@ async function bootstrap() {
   const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
 
-  if(process.env.NODE_ENV === 'development')
+  if(process.env.NODE_ENV === 'development'){
     app.enableCors();//Every cors
+  }else{
+    app.enableCors({origin: serverConfig.origin})
+    logger.log(`Accepting requests from origin: ${serverConfig.origin}`)
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
