b20b42e44abfd9d1f761f3c0b31085f3db312502
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-24 18:24:20 -0400

    congiguration env prod-dev-def

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from "@nestjs/common";
 import { AppModule } from './app.module';
+import * as config from 'config'
 
 async function bootstrap() {
   
-  console.log('---Start:Logging---');
+  const serverConfig = config.get('server')
   const logger = new Logger('bootstrap')
-  console.log('---End:Logging---');
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Aplication listening on port ${port}`)
 }
